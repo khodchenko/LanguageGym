e23359d7d0ab4c45782b2e0f863a376b33960e08
khodchenko <khodch3nk0@gmail.com>
2023-05-11 20:19:52 +0200

    fixed problems with recyclerview

--- app/src/main/java/com/example/languagegym/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/languagegym/ui/home/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -41,19 +40,29 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
         recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         loadingIndicator = binding.progressBar
         fab = binding.fab
         fab.hide()
 
-        // создаем экземпляр DictionaryDatabaseHelper
         databaseHelper = DictionaryDatabaseHelper(requireContext())
 
-        // показываем индикатор загрузки
         showLoadingIndicator()
 
+        GlobalScope.launch(Dispatchers.IO) {
+            val words = databaseHelper.getAllWords()
+            withContext(Dispatchers.Main) {
+                setupRecyclerView(words)
+                hideLoadingIndicator()
+                fab.show()
+            }
+        }
 
+        fab.setOnClickListener {
+            showAddWordDialog()
+        }
 
         return binding.root
     }
@@ -70,20 +79,7 @@ class HomeFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-   // запускаем загрузку списка в отдельном потоке
-        GlobalScope.launch(Dispatchers.IO) {
-            val words = databaseHelper.getAllWords()
-            withContext(Dispatchers.Main) {
 
-
-                // инициализируем RecyclerView
-                setupRecyclerView(words)
-                // скрываем индикатор загрузки
-                hideLoadingIndicator()
-                // делаем FloatingActionButton активной
-                fab.show()
-            }
-        }
 
     }
 
@@ -139,47 +135,25 @@ class HomeFragment : Fragment() {
         builder.create().show()
     }
 
-    private val onWordItemClickListener = object : RecyclerViewAdapter.OnWordItemClickListener {
-        override fun onWordItemClick(word: WordModel) {
-            // Обработка события клика на элементе списка
-        }
-
-        override fun onWordItemDeleteClick(word: WordModel) {
-            // Обработка события удаления элемента списка
-        }
-    }
-
     private fun setupRecyclerView(words: List<WordModel>) {
-        // создаем адаптер для RecyclerView
-        val adapter = RecyclerViewAdapter(requireContext(), words, object : RecyclerViewAdapter.OnWordItemClickListener {
+        adapter = RecyclerViewAdapter(requireContext(), words, object : RecyclerViewAdapter.OnWordItemClickListener {
 
             override fun onWordItemClick(word: WordModel) {
-                //todo remake and use SafeArgs
                 val bundle = Bundle().apply {
                     putParcelable("word", word)
                 }
                 findNavController().navigate(R.id.action_nav_home_to_detailsFragment, bundle)
-
             }
 
             override fun onWordItemDeleteClick(word: WordModel) {
                 // todo implementation
             }
         })
-        // устанавливаем адаптер в RecyclerView
         recyclerView.adapter = adapter
-
-        fab.setOnClickListener {
-            showAddWordDialog()
-        }
     }
 
     private fun updateRecyclerView(allWords: List<WordModel>) {
-        // обновляем данные в адаптере
         adapter.updateData(allWords)
-
-        // инициализируем RecyclerView
-        setupRecyclerView(allWords)
     }
 
     override fun onDestroyView() {
