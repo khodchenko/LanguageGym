584a5f02952112d47bde421e511cc282f07979b2
Vedushka <55571275+Vedushka@users.noreply.github.com>
2023-05-22 23:19:01 +0200

    Update CategoryModel.kt
    Для категорий хорошо бы тоже иметь первичный ключ. Это что-бы связь огранизовать между категориями и словами

--- app/src/main/java/com/example/languagegym/ui/model/CategoryModel.kt
+++ app/src/main/java/com/example/languagegym/ui/model/CategoryModel.kt
@@ -1,6 +1,8 @@
 package com.example.languagegym.ui.model
 
 data class CategoryModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val name: String,
     val categoryColor: Int,
     val words: List<WordModel>
